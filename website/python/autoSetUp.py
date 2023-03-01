8c382139dbb5d577bf0ea7b89112020f9e116cde
Luca Schirmer <acu1410l@gmail.com>
2023-03-01 19:30:48 +0100

    Update autoSetUp.py

--- website/python/autoSetUp.py
+++ website/python/autoSetUp.py
@@ -1,9 +1,13 @@
 class website:
-    def __init__(self, title, aboutText, email, address, contact) -> None:
+    def __init__(self, title, aboutText, email, address, contact, mainColor, secondaryColor, accentColor, backgroundColor) -> None:
         self.title = title
         self.aboutText = aboutText
         self.email = email
         self.address = address
+        self.contact = contact
+        self.mainColor = mainColor
+        self.secondaryColor = secondaryColor
+        self.contact = contact
         self.contact = contact
 
 
