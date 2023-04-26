16538d615716a423641a480b2487e84e93d1355a
Luca Schirmer <acu1410l@gmail.com>
2023-04-26 18:22:52 +0200

    update auto set up

--- website/python/autoSetUp.py
+++ website/python/autoSetUp.py
@@ -1,5 +1,6 @@
 import os
 import json
+import codecs
 
 class website:
     def __init__(self, title, aboutText, mainColor, secondaryColor, accentColor, backgroundColor) -> None:
@@ -15,7 +16,8 @@ class website:
 
 
 class project: 
-    def __init__(self, text, foto) -> None:
+    def __init__(self, title, text, foto) -> None:
+        self.title = title
         self.text = text
         self.foto = foto
         
@@ -61,83 +63,89 @@ backgroundColor = input('\nEnter the background color\n')
 
 protoWebsite = website(title, aboutText, mainColor, secondaryColor, accentColor, backgroundColor)
 
-print(protoWebsite.toJSON())
-
-
-
 print("\n\n")
-print("Add Projects to your Website \n")
-
-bool = "Y"
-
-projectArr = []
+print("**************** Add Projects to your Website ****************\n")
 
 
-def testForN():
+projectArr = {}
+
+
+def testForN(bool):
     if (bool != "Y" or bool != "y"):
         bool = input("Press Y to add Projects\n\n")
-        if(bool != "N" or bool != "n"):
-            bool = "N"
-        else:
-            testForN()         
+    elif(bool == "N" or bool == "n"):
+        bool = "N"
+    elif(bool == "Y" or bool == "y"):
+        bool = "Y"
+    else:
+        testForN(bool)         
 
+
+
+bool = "Y"
+i = 0
 while(bool == "Y" or bool == "y"):
-    _project = input("Add a text for your project\n")
+    _project = input("Add a title for your project\n")
+    _projectText = input("Add a text for your project\n")
     _fotoUrl = input("Add a photoURL for your project\n")
-    listItemProject = project(_project, _fotoUrl)
-    
-    projectArr.append(listItemProject.toJSON())
-    print(listItemProject.toJSON())
+    listItemProject = project(_project ,_projectText, _fotoUrl)
+    projectArr[i] = listItemProject.toJSON()
+    i += 1
     bool = input("Press Y to add Projects\n\n")
-    testForN()
+    testForN(bool)
+
+
+socialMediaDict = dict()
+
+_boolFacebook = input("Press Y to add facebook to your social media\n")
+if(_boolFacebook  != "N" and _boolFacebook != "n"):
+    _linkFacebook =  input("Enter the link for your social media\n")
+    socialMediaDict['facebook'] = _linkFacebook
     
-while(bool == "Y" or bool == "y"):
-    socialMediaDict = dict()
+_boollinkedin = input("Press Y to add linkedin toy our social media\n")
+if(_boollinkedin != "N" and _boollinkedin != "n"):
+    _linklinkedin =  input("Enter the link for your social media\n")
+    socialMediaDict['linkedin'] = _linklinkedin
+
+_boolpinterest = input("Press Y to add pinterest toy our social media\n")
+if(_boolpinterest  != "N" and _boolpinterest != "n"):
+    _linkpinterest =  input("Enter the link for your social media\n")
+    socialMediaDict['pinterest'] = _linkpinterest
     
-    _boolFacebook = input("Press Y to add facebook to your social media\n")
-    if(_boolFacebook):
-        _linkFacebook =  input("Enter the link for your social media\n")
-        socialMediaDict['facebook'] = _linkFacebook
-        
-    _boollinkedin = input("Press Y to add linkedin toy our social media\n")
-    if(_boollinkedin):
-        _linklinkedin =  input("Enter the link for your social media\n")
-        socialMediaDict['linkedin'] = _linklinkedin
+_booltwitter = input("Press Y to add twitter toy our social media\n")
+if(_booltwitter != "N" and _booltwitter != "n"):
+    _linktwitter =  input("Enter the link for your social media\n")
+    socialMediaDict['twitter'] = _linktwitter
     
-    _boolpinterest = input("Press Y to add pinterest toy our social media\n")
-    if(_boolpinterest):
-        _linkpinterest =  input("Enter the link for your social media\n")
-        socialMediaDict['pinterest'] = _linkpinterest
-        
-    _booltwitter = input("Press Y to add twitter toy our social media\n")
-    if(_booltwitter):
-        _linktwitter =  input("Enter the link for your social media\n")
-        socialMediaDict['twitter'] = _linktwitter
-        
-    boolwhatsapp = input("Press Y to add whatsapp toy our social media\n")
-    if(boolwhatsapp):
-        _linkwhatsapp =  input("Enter the link for your social media\n")
-        socialMediaDict['whatsapp'] = _linkwhatsapp
-        
-    boolyoutube= input("Press Y to add youtube toy our social media\n")
-    if(boolyoutube):
-        _linkyoutube =  input("Enter the link for your social media\n")
-        socialMediaDict['youtube'] = _linkyoutube
-        
-    boolinstagramm= input("Press Y to add instagramm toy our social media\n")
-    if(boolinstagramm):
-        _linkinstagramm =  input("Enter the link for your social media\n")
-        socialMediaDict['instagramm'] = _linkinstagramm
-        
-    boolgithub = input("Press Y to add github toy our social media\n")
-    if(boolgithub):
-        _linkgithub =  input("Enter the link for your social media\n")
-        socialMediaDict['github'] = _linkgithub
-   
+boolwhatsapp = input("Press Y to add whatsapp toy our social media\n")
+if(boolwhatsapp != "N" and boolwhatsapp != "n"):
+    _linkwhatsapp =  input("Enter the link for your social media\n")
+    socialMediaDict['whatsapp'] = _linkwhatsapp
+    
+boolyoutube= input("Press Y to add youtube toy our social media\n")
+if(boolyoutube  != "N" and boolyoutube != "n"):
+    _linkyoutube =  input("Enter the link for your social media\n")
+    socialMediaDict['youtube'] = _linkyoutube
+    
+boolinstagramm= input("Press Y to add instagramm toy our social media\n")
+if(boolinstagramm  != "N" and boolinstagramm != "n"):
+    _linkinstagramm =  input("Enter the link for your social media\n")
+    socialMediaDict['instagramm'] = _linkinstagramm
+    
+boolgithub = input("Press Y to add github toy our social media\n")
+if(boolgithub != "N" and boolgithub != "n"):
+    print
+    _linkgithub =  input("Enter the link for your social media\n")
+    socialMediaDict['github'] = _linkgithub
+
+jsonObject = {
+    "website": protoWebsite.toJSON(),
+    "projects": json.dumps(projectArr),
+    "contact": json.dumps(socialMediaDict)
+}   
    
 with open("data.json", "w") as dataJson:
-    dataJson.write(protoWebsite.toJSON())
-    dataJson.write(json.dumps(projectArr, default=lambda o: o.__dict__, indent=4))
-    dataJson.write(json.dumps(socialMediaDict, default=lambda o: o.__dict__, indent=4))
+   dataJson.write(json.dumps(jsonObject)),
+
 
 input('')
