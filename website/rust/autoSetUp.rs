a5c718626ca0a00f45a96d2df2c126d916ade7f0
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2023-06-16 20:58:57 +0200

    Update autoSetUp.rs

--- website/rust/autoSetUp.rs
+++ website/rust/autoSetUp.rs
@@ -13,6 +13,8 @@ fn create_files(directoryName: String){
     let mut project_folder = fs::create_dir(directoryName);
 
     let mut index_html_file = fs::create("index.html").expect("creation failed");
+    let mut index_html_file = fs::create("portfolio.html").expect("creation failed");
+    let mut index_html_file = fs::create("main.css").expect("creation failed");
     index_html_file.write("das ist ein Test").expect("write failed");
 
 }
