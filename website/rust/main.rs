92fc7b640f40350a83612451392511e7e793df96
Luca Schirmer <acu1410l@gmail.com>
2023-06-20 00:59:02 +0200

    update auto set up with rust

--- website/rust/main.rs
+++ website/rust/main.rs
@@ -3,20 +3,20 @@ use std::fs;
 use std::io::Write;
 
 fn main(){
-    println("init Programm");
+    println!("init Programm");
 
     create_files("project");
 }
 
-fn create_files(directoryName: String){
+fn create_files(directoryName: &str){
         
-    let mut project_folder = fs::create_dir(directoryName);
-    let mut script_folder = fs::create_dir(directoryName + "/script");
+    let project_folder = fs::create_dir(directoryName);
+    let script_folder = fs::create_dir(directoryName.to_owned() + "/script");
 
-    let mut index_html_file = fs::create("index.html").expect("creation failed");
-    let mut index_html_file = fs::create("portfolio.html").expect("creation failed");
-    let mut index_html_file = fs::create("main.css").expect("creation failed");
-    index_html_file.write("das ist ein Test").expect("write failed");
+    let mut index_html_file = fs::File::create("index.html").expect("creation failed");
+    let portfolio_html_file = fs::File::create("portfolio.html").expect("creation failed");
+    let main_css_file = fs::File::create("main.css").expect("creation failed");
+    index_html_file.write(b"das ist ein Test").expect("write failed");
 
 }
 
