06720bdbd69d703efa645e8a7177eaeec6b98edf
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2023-06-20 21:45:19 +0200

    init content script files

--- website/rust/main.rs
+++ website/rust/main.rs
@@ -12,6 +12,9 @@ fn create_files(directoryName: &str){
         
     let project_folder = fs::create_dir(directoryName);
     let script_folder = fs::create_dir(directoryName.to_owned() + "/script");
+    
+    // script files
+    let main_js_file = fs::File::create("/script/".to_owned() + "main.js");
 
     let mut index_html_file = fs::File::create("index.html").expect("creation failed");
     let portfolio_html_file = fs::File::create("portfolio.html").expect("creation failed");
