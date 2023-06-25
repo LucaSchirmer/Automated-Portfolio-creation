6d9553b863ba084aec2aa7dc69b32d6350118728
Luca Schirmer <acu1410l@gmail.com>
2023-06-25 17:47:14 +0200

    init bertter rust structure

---
+++ website/rust/target/debug/deps/native_tls-0e512db9f4462a63.d
@@ -0,0 +1,8 @@
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\native_tls-0e512db9f4462a63.rmeta: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\lib.rs C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\imp\schannel.rs
+
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\libnative_tls-0e512db9f4462a63.rlib: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\lib.rs C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\imp\schannel.rs
+
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\native_tls-0e512db9f4462a63.d: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\lib.rs C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\imp\schannel.rs
+
+C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\lib.rs:
+C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\native-tls-0.2.11\src\imp\schannel.rs:
---
+++ website/rust/target/debug/deps/windows_targets-55a38d4077b42eae.d
@@ -0,0 +1,7 @@
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\windows_targets-55a38d4077b42eae.rmeta: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.48.0\src\lib.rs
+
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\libwindows_targets-55a38d4077b42eae.rlib: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.48.0\src\lib.rs
+
+C:\Users\LucaS\Desktop\programmieren_code\html\Automated-Portfolio-creation\website\rust\target\debug\deps\windows_targets-55a38d4077b42eae.d: C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.48.0\src\lib.rs
+
+C:\Users\LucaS\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.48.0\src\lib.rs:
